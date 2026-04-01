377d62de0de67c1991fc4c1447d528ff9ea3ca6e
Lycan-Xx <msbello514@gmail.com>
2026-04-01 07:22:30 +0100

    refactor: remove Sanity CMS integration files
    Removes all Sanity CMS related files from the project including client configuration, schemas, and studio setup as part of the migration to a JSON-based admin panel.

--- sanitycms/sanity.cli.js
+++
@@ -1,9 +0,0 @@
-// studio/sanity.cli.js  
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: 'yeizgznh', // Same as above
-    dataset: 'production'
-  }
-})
